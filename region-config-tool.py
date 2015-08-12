bb00dd825f8a3c6c05eae1fc9c966f4f6f2f9ad9
Harold Spencer Jr <hspencer77@gmail.com>
2015-08-12 09:45:53 -0400

    Applied PEP8 standards

--- region-config-tool.py
+++ region-config-tool.py
@@ -18,46 +18,50 @@ def get_options():
         """
         Function to print standard formatting error for each region parameter
         provided.
-       
+
         param:  err_string:  variable that has the incorrect format
         """
-        msg = ("\nIncorrect Region \'key=value\' format - " + err_string + "\nPlease use "
+        msg = ("\nIncorrect Region \'key=value\' format - " +
+               err_string + "\nPlease use "
                + "the following format: " + "\n" + "\t" +
                "region_name=ParameterValue,ip=ParameterValue," +
-               "domain_name=ParameterValue") 
+               "domain_name=ParameterValue")
         return msg
 
     def region_check(region):
         """
         Function to check formatting of parameters passed for each region
-        
-        param:  region:  string that should contain the following format - 
+
+        param:  region:  string that should contain the following format -
                 region_name=ParameterValue,cloud_cert=ParameterValue,domain_name=ParameterValue
         """
         # Check each region has correct number of parameters
         try:
-            region_name, cloud_cert, domain_name = region.split(',')        
+            region_name, cloud_cert, domain_name = region.split(',')
         except ValueError:
             msg = format_error(region)
             raise argparse.ArgumentTypeError(msg)
-        
+
         # Check that each parameter is the proper key=value format
-        for param in [ region_name, cloud_cert, domain_name ]:
+        for param in [region_name, cloud_cert, domain_name]:
             try:
                 key_param, value_param = param.split('=')
-            except ValueError: 
+            except ValueError:
                 msg = format_error(region)
                 raise argparse.ArgumentTypeError(msg)
-            
-            if not any(key_param in key for key in ["region_name", "cloud_cert", "domain_name"]):
+
+            if not any(key_param in key for key in
+                       ["region_name", "cloud_cert", "domain_name"]):
                 msg = format_error(region)
                 raise argparse.ArgumentTypeError(msg)
 
         # Check for proper length and valid region name
         region_key, region_value = region_name.split('=')
-        if not re.match("^(?![0-9]+$)(?!-)[a-zA-Z0-9-]{,63}(?<!-)$", region_value):
+        if not re.match("^(?![0-9]+$)(?!-)[a-zA-Z0-9-]{,63}(?<!-)$",
+                        region_value):
             msg = format_error(region)
-            region_msg = ("\n\n" + region_key + " has a value which is not a valid DNS label." +
+            region_msg = ("\n\n" + region_key +
+                          " has a value which is not a valid DNS label." +
                           " Please refer to <label> definition for RFC 1035 " +
                           "https://www.ietf.org/rfc/rfc1035.txt.")
             raise argparse.ArgumentTypeError(msg + region_msg)
@@ -66,17 +70,21 @@ def get_options():
         cloud_key, cloud_value = cloud_cert.split('=')
         if not os.path.isfile(cloud_value):
             msg = format_error(region)
-            cloud_msg = ("\n\n" + cloud_key + " has a value which is not a file.")
+            cloud_msg = ("\n\n" + cloud_key +
+                         " has a value which is not a file.")
             raise argparse.ArgumentTypeError(msg + cloud_msg)
 
-        # Check for proper domain/subdomain format 
+        # Check for proper domain/subdomain format
         domain_key, domain_value = domain_name.split('=')
         if not re.findall(
-                         r'^[a-zA-Z\d-]{,63}(\.[a-zA-Z\d-]{,63})*$',
-                         domain_value):
+                r'^[a-zA-Z\d-]{,63}(\.[a-zA-Z\d-]{,63})*$',
+                domain_value
+                ):
             msg = format_error(region)
-            domain_msg = ("\n\n" + domain_key + " has a value which is not a value DNS domain. " +
-                          "Please refer to the <domain> definition in RFC 1035 " +
+            domain_msg = ("\n\n" + domain_key +
+                          " has a value which is not a value DNS domain. " +
+                          "Please refer to the " +
+                          "<domain> definition in RFC 1035 " +
                           "https://www.ietf.org/rfc/rfc1035.txt.")
             raise argparse.ArgumentTypeError(msg + domain_msg)
 
@@ -85,7 +93,7 @@ def get_options():
     def http_check(protocol):
         """
         Function to confirm if protocol is either http or https
-  
+
         param:  protocol: Define http or https protocol
         """
         if not re.match('^http$|^https$', protocol, re.I):
@@ -98,8 +106,15 @@ def get_options():
     def digest_check(digest):
         """
         Function to confirm if digest meets the supported formats described
-        here:  https://www.openssl.org/docs/apps/openssl.html#MESSAGE-DIGEST-COMMANDS
-        Formats supported by Eucalyptus Federation: sha1, sha224, sha256, sha384, sha512
+        here:
+        https://docs.oracle.com/javase/7/docs/technotes/
+        guides/security/StandardNames.html#MessageDigest
+        Formats supported by Eucalyptus Federation:
+            - sha1
+            - sha224
+            - sha256
+            - sha384
+            - sha512
 
         param: digest: OpenSSL digest algorithm
         """
@@ -112,41 +127,64 @@ def get_options():
                    "Default value: sha256")
             raise argparse.ArgumentTypeError(msg)
         return digest
-                   
+
     parser = argparse.ArgumentParser(prog="region-config-tool.py",
-                 usage='%(prog)s REGION [REGION ...] [-f | --filename] FILE_NAME',
-                 description='Script that creates region configuration file from \
-                 multiple region (i.e. cloud) parameters that are intended to be \
-                 part of a federated environment.')
-    region_group = parser.add_argument_group("Region Arguments", "Arguments to define multiple regions")
-    region_group.add_argument('region', nargs='+', type=region_check, 
+                                     usage="%(prog)s REGION [REGION ...] " +
+                                     "[-f | --filename] FILE_NAME",
+                                     description='Script that creates region \
+                                     configuration file from \
+                                     multiple region (i.e. cloud) parameters \
+                                     that are intended to be \
+                                     part of a federated environment.')
+    region_group = parser.add_argument_group("Region Arguments",
+                                             "Arguments to " +
+                                             "define multiple regions")
+    region_group.add_argument('region', nargs='+', type=region_check,
                               help='Region defintion; \
                               multiple regions separated by spaces; should \
                               contain the following format for each entry: \
-                              region_name=ParameterValue,cloud_cert=ParameterValue,domain_name=ParameterValue.\
-                              For each cloud (i.e. region) - \"region_name\" should match \
-                              region.region_name cloud property; \"cloud_cert\" should be \
-                              the cloud certificate (/var/lib/eucalyptus/keys/cloud-cert.pem) of the Cloud \
-                              Controller for that cloud (i.e. region); \"domain_name\" should match the \
+                              region_name=ParameterValue,cloud_cert=ParameterValue,\
+                              domain_name=ParameterValue.\
+                              For each cloud (i.e. region) - \
+                              \"region_name\" should match \
+                              region.region_name cloud property; \
+                              \"cloud_cert\" should be \
+                              the cloud certificate \
+                              (/var/lib/eucalyptus/keys/cloud-cert.pem) \
+                              of the Cloud Controller \
+                              for that cloud (i.e. region);\
+                              \"domain_name\" should match the \
                               system.dns.dnsdomain cloud property')
-    file_group = parser.add_argument_group("Region Configuration File Argument", 'Argument to define name of \
-                                           of generated region configuration file.') 
-    file_group.add_argument('-f', '--filename', dest='file_name', 
+    file_group = parser.add_argument_group("Region Configuration " +
+                                           "File Argument",
+                                           'Argument to define name of \
+                                           of generated region \
+                                           configuration file.')
+    file_group.add_argument('-f', '--filename', dest='file_name',
                             type=argparse.FileType('w'),
-                            help="Generated region configuration file.")
-    http_option = parser.add_argument_group("HTTP/HTTPS Protocol Flag Argument",
-                                            "Argument to set HTTP or HTTPS for Service Endpoints")
+                            help='Generated region \
+                            configuration file.')
+    http_option = parser.add_argument_group("HTTP/HTTPS Protocol " +
+                                            "Flag Argument",
+                                            'Argument to set HTTP or HTTPS \
+                                            for Service Endpoints')
     http_option.add_argument('-p', '--protocol', dest='protocol',
                              type=http_check, default='http',
-                             help="Flag to use HTTP/HTTPS for Service Endpoints. Default: http")
-    cert_fingerprint = parser.add_argument_group("OpenSSL Signature Algorithm Argument",
-                                                 "Argument that defines what OpenSSL signing alorithm " +
-                                                 "was used for the cloud certificate")
+                             help='Flag to use HTTP/HTTPS \
+                             for Service Endpoints. Default: http')
+    cert_fingerprint = parser.add_argument_group("OpenSSL Signature " +
+                                                 "Algorithm Argument",
+                                                 'Argument that defines what \
+                                                 OpenSSL signing alorithm \
+                                                 was used for the \
+                                                 cloud certificate')
     cert_fingerprint.add_argument('-d', '--digest', dest='digest',
                                   type=digest_check, default='sha256',
-                                  help="Digest algorithm used to sign cloud certificate. Default: sha256")
+                                  help="Digest algorithm used to sign cloud " +
+                                  "certificate. Default: sha256")
     options = parser.parse_args()
     return options
+
 
 def local(cmd):
     """
@@ -155,7 +193,8 @@ def local(cmd):
     param:  cmd:  command to be executed in the shell
     """
     args = cmd.split()
-    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=4096)
+    process = subprocess.Popen(args, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, bufsize=4096)
     output, unused_err = process.communicate()
     retcode = process.poll()
     if retcode:
@@ -167,6 +206,7 @@ def local(cmd):
         raise error
     return output.split("\n")
 
+
 def verify_fingerprint(digest, cloud_cert):
     """
     Function to grab fingerprint using digest and cloud certificate
@@ -174,11 +214,11 @@ def verify_fingerprint(digest, cloud_cert):
     param:  digest:  OpenSSL digest algorithm
     param:  cloud_cert:  Eucalyptus Cloud Certificate
     """
-    fingerprint_cmd = ("openssl x509 -inform PEM -in " + cloud_cert + 
+    fingerprint_cmd = ("openssl x509 -inform PEM -in " + cloud_cert +
                        " -noout -fingerprint -" + digest)
     fingerprint_check = local(fingerprint_cmd)
-    return fingerprint_check[0].split('=')[1]    
-    
+    return fingerprint_check[0].split('=')[1]
+
 
 if __name__ == "__main__":
     # Grab commandline options for region configuration
@@ -190,7 +230,7 @@ if __name__ == "__main__":
     region_config = {'Regions': []}
     for region in options.region:
         """
-        For each region entry, grab 'region_name', 'cloud_certificate', 
+        For each region entry, grab 'region_name', 'cloud_certificate',
         and 'domain_name'
         """
         region_name, cloud_cert, domain_name = region.split(',')
@@ -201,19 +241,21 @@ if __name__ == "__main__":
             'Name': region_name.split('=')[1],
             'CertificateFingerprintDigest': digest_alg,
             'CertificateFingerprint': digest_fingerprint,
-            'IdentifierPartitions': [ identifier ],
+            'IdentifierPartitions': [identifier],
             'Services': [
-                {
-                 'Type': 'identity',
-                 'Endpoints': [ options.protocol + '://identity.' + domain_name.split('=')[1] + ':8773/' ]
-                },
-                {
-                 'Type': 'compute',
-                 'Endpoints': [ options.protocol + '://compute.' + domain_name.split('=')[1] + ':8773/' ]
-                }
-             ]
-         })
-           
+                {'Type': 'identity',
+                 'Endpoints': [options.protocol +
+                               '://identity.' +
+                               domain_name.split('=')[1] +
+                               ':8773/']},
+                {'Type': 'compute',
+                 'Endpoints': [options.protocol +
+                               '://compute.' +
+                               domain_name.split('=')[1] +
+                               ':8773/']}
+            ]
+        })
+
     # Dump JSON data into file
     try:
         with options.file_name as json_file:
@@ -221,4 +263,4 @@ if __name__ == "__main__":
                       indent=4, separators=(',', ': '))
         json_file.closed
     except:
-        raise AttributeError("Missing -f|--filename option") 
+        raise AttributeError("Missing -f|--filename option")
